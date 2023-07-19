bd72224a5ade74c41056a60790c72e6a66d30c18
stla <laurent_step@yahoo.fr>
2023-07-19 20:08:55 +0200

    removed messages in zzz

--- R/zzz.R
+++ R/zzz.R
@@ -4,19 +4,10 @@ NULL
 
 .onLoad <- function(libname, pkgname) {
 
-  has_internet  <- pingr::is_online()
   fourier_is_up <- pingr::is_up("www-fourier.univ-grenoble-alpes.fr")
   no_giac       <- !.giac_is_installed()
 
-  if(no_giac && (!has_internet || !fourier_is_up)) {
-    message(
-      "Giac is not installed and there's no internet connection or the ",
-      "website is not reachable."
-    )
-  }
-
   if(fourier_is_up && no_giac) {
-    message("Downloading Giac.")
     .giac_download()
   }
 
