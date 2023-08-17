b504145b1006b423462794c32308d848efdbc8af
stla <laurent_step@yahoo.fr>
2023-08-18 01:15:23 +0200

    Sys.unsetenv("HTTP_PROXY") on .onLoad()

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,8 @@ NULL
 
 .onLoad <- function(libname, pkgname) {
 
+  Sys.unsetenv("HTTP_PROXY")
+
   fourier_is_up <- pingr::is_up("www-fourier.univ-grenoble-alpes.fr")
   no_giac       <- !.giac_is_installed()
 
